04100b657f6f99a261dd148ad1d285a714e1e112
Benjamin Tolman <bentlman1981@gmail.com>
2022-06-21 11:59:47 -0400

    Initial Commit
    Initial Commit

---
+++ JSON API for Windows Forms/DVPProjectBored/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace DVPProjectBored
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_GetIdea = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmb_Category = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.num_Participants = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmb_Price = new System.Windows.Forms.ComboBox();
+            this.txt_Idea = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmb_Acc = new System.Windows.Forms.ComboBox();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Participants)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_GetIdea
+            // 
+            this.btn_GetIdea.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_GetIdea.Location = new System.Drawing.Point(51, 331);
+            this.btn_GetIdea.Name = "btn_GetIdea";
+            this.btn_GetIdea.Size = new System.Drawing.Size(162, 46);
+            this.btn_GetIdea.TabIndex = 0;
+            this.btn_GetIdea.Text = "Get Idea";
+            this.btn_GetIdea.UseVisualStyleBackColor = true;
+            this.btn_GetIdea.Click += new System.EventHandler(this.btn_GetIdea_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(51, 93);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(54, 29);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 25);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(162, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // cmb_Category
+            // 
+            this.cmb_Category.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Category.FormattingEnabled = true;
+            this.cmb_Category.Items.AddRange(new object[] {
+            "Random",
+            "Busywork",
+            "Charity",
+            "Cooking",
+            "DIY",
+            "Education",
+            "Recreation",
+            "Relaxation",
+            "Social"});
+            this.cmb_Category.Location = new System.Drawing.Point(162, 140);
+            this.cmb_Category.Name = "cmb_Category";
+            this.cmb_Category.Size = new System.Drawing.Size(162, 28);
+            this.cmb_Category.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Category";
+            // 
+            // num_Participants
+            // 
+            this.num_Participants.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num_Participants.Location = new System.Drawing.Point(204, 189);
+            this.num_Participants.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.num_Participants.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num_Participants.Name = "num_Participants";
+            this.num_Participants.Size = new System.Drawing.Size(120, 26);
+            this.num_Participants.TabIndex = 6;
+            this.num_Participants.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Participants";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Max Cost";
+            // 
+            // cmb_Price
+            // 
+            this.cmb_Price.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Price.FormattingEnabled = true;
+            this.cmb_Price.Items.AddRange(new object[] {
+            "Any",
+            "Free",
+            "Low",
+            "Medium",
+            "High"});
+            this.cmb_Price.Location = new System.Drawing.Point(162, 234);
+            this.cmb_Price.Name = "cmb_Price";
+            this.cmb_Price.Size = new System.Drawing.Size(162, 28);
+            this.cmb_Price.TabIndex = 8;
+            // 
+            // txt_Idea
+            // 
+            this.txt_Idea.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Idea.Location = new System.Drawing.Point(51, 394);
+            this.txt_Idea.Multiline = true;
+            this.txt_Idea.Name = "txt_Idea";
+            this.txt_Idea.Size = new System.Drawing.Size(273, 101);
+            this.txt_Idea.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(51, 514);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 42);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Favorite";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(196, 514);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 42);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Dislike";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(47, 284);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Accessibility";
+            // 
+            // cmb_Acc
+            // 
+            this.cmb_Acc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Acc.FormattingEnabled = true;
+            this.cmb_Acc.Items.AddRange(new object[] {
+            "Any",
+            "Easy",
+            "Medium",
+            "Hard"});
+            this.cmb_Acc.Location = new System.Drawing.Point(162, 281);
+            this.cmb_Acc.Name = "cmb_Acc";
+            this.cmb_Acc.Size = new System.Drawing.Size(162, 28);
+            this.cmb_Acc.TabIndex = 14;
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Reset.Location = new System.Drawing.Point(219, 331);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(105, 46);
+            this.btn_Reset.TabIndex = 16;
+            this.btn_Reset.Text = "Reset Form";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(379, 665);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmb_Acc);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt_Idea);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmb_Price);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.num_Participants);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmb_Category);
+            this.Controls.Add(this.btn_GetIdea);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Participants)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_GetIdea;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ComboBox cmb_Category;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown num_Participants;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmb_Price;
+        private System.Windows.Forms.TextBox txt_Idea;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmb_Acc;
+        private System.Windows.Forms.Button btn_Reset;
+    }
+}
+
---
+++ JSON API for Windows Forms/DVPProjectBored/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace DVPProjectBored
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            HandleClientWindowSize();
+
+            //Set the initial values of the form controls.
+            cmb_Category.Text = "Random";
+            num_Participants.Value = 1;
+            cmb_Price.Text = "Any";
+            cmb_Acc.Text = "Any";
+        }
+
+        WebClient webConnection = new WebClient();
+
+        //Returns a random activity if nothing is added.
+        string baseAPI = ("http://www.boredapi.com/api/activity?");
+
+        //Used to hold the decided options as a finished string to attach to baseAPI.
+        string fullAPI = "";
+    
+        //This holds the returned api object.
+        JObject returnedJSON = new JObject();
+
+        //Creates the fullAPI string by getting the form data.
+        //and putting them into the correct format.
+        private void CreateAPI()
+        {
+            string category = "";
+            string participants = "";
+            string price = "";
+            string access = "";
+
+            if (cmb_Category.Text != "Random"){ category = $@"type={cmb_Category.Text.ToLower()}";}
+
+            switch(cmb_Price.Text)
+            {
+                case "Any":
+                    price = @"&minprice=0&maxprice=1.0";
+                    break;
+                case "Free":
+                    price = @"&minprice=0&maxprice=0.0";
+                    break;
+                case "Low":
+                    price = @"&minprice=0&maxprice=0.2";
+                    break;
+                case "Medium":
+                    price = @"&minprice=0&maxprice=0.5";
+                    break;
+                case "High":
+                    price = @"&minprice=0&maxprice=1";
+                    break;
+            }
+
+            switch (cmb_Acc.Text)
+            {
+                case "Any":
+                    access = @"&minaccessibility=0&maxaccessibility=1.0";
+                    break;
+                case "Easy":
+                    access = @"&minaccessibility=0&maxaccessibility=0.3";
+                    break;
+                case "Medium":
+                    access = @"&minaccessibility=0&maxaccessibility=0.6";
+                    break;
+                case "Hard":
+                    access = @"&minaccessibility=0&maxaccessibility=0.1";
+                    break;
+            }
+
+            decimal part = num_Participants.Value;
+            participants = $@"&participants={part.ToString()}";
+
+            //Build the api string.
+            fullAPI = $"{category}" + $"{participants}" + $"{price}" + $"{access}";
+
+            //Combine the base and full api for sending.
+            fullAPI = baseAPI + fullAPI;
+
+        }
+
+        //Create call and send to web.
+        private void btn_GetIdea_Click(object sender, EventArgs e)
+        {
+            CreateAPI();
+            ReadData();
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            cmb_Category.Text = "Random";
+            num_Participants.Value = 1;
+            cmb_Price.Text = "Any";
+            cmb_Acc.Text = "Any";
+        }
+
+        //Get and read JSON data
+        private void ReadData()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                webConnection = new WebClient();
+
+                string apiData = fullAPI;
+
+                try
+                {
+                    apiData = webConnection.DownloadString(fullAPI);
+                }
+                catch (WebException e)
+                {
+                    MessageBox.Show("Failed to Connect");
+                    return;
+                }
+
+                //Parse data string to jObject
+                returnedJSON = JObject.Parse(apiData);
+
+                //Show in the text area the returned activity or the error text.
+                if(returnedJSON.ContainsKey("activity"))
+                {
+                    txt_Idea.Text = ("Activity: " + returnedJSON["activity"].ToString() +".");
+                }
+                else
+                {
+                    txt_Idea.Text = "BoredAPI is limited at this time and has not found an activity for you with the selected perameters...";
+                }
+            }
+        }
+        
+       
+        void HandleClientWindowSize()
+        {
+            float HeightValueToChange = 1.4f;
+            float WidthValueToChange = 6.0f;
+
+            int height = Convert.ToInt32(Screen.PrimaryScreen.WorkingArea.Size.Height / HeightValueToChange);
+            int width = Convert.ToInt32(Screen.PrimaryScreen.WorkingArea.Size.Width / WidthValueToChange);
+            if (height < Size.Height)
+                height = Size.Height;
+            if (width < Size.Width)
+                width = Size.Width;
+            this.Size = new Size(width, height);
+            //this.Size = new Size(376, 720);
+        }
+    }
+}
